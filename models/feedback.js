6bbbf3689a823f1b0c3eae0cb4f28b111c8e22ac
eklavyakaushik <160946194+eklavyakaushik@users.noreply.github.com>
2025-01-25 14:21:08 +0530

    Add files via upload

---
+++ models/feedback.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const FeedbackSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  contactNumber: { type: String, required: true },
+  email: { type: String, required: true },
+  message: { type: String, required: true },
+});
+module.exports = mongoose.model("Feedback", FeedbackSchema);
